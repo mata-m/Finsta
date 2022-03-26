f9c40ce8aa110671d307f14201b7cc95076657db
mata-m <m.mata3698@gmail.com>
2022-03-25 20:48:03 -0700

    Version 1, minimal features

---
+++ Instagram/Secrets.swift
@@ -0,0 +1,11 @@
+//
+//  Secrets.swift
+//  Instagram
+//
+//  Created by Mark on 3/23/22.
+//
+
+import Foundation
+
+let APP_ID = "NhdoIevi2OQRs9VCURHFXyIy6rgI8JojvFKbIBoj"
+let CLIENT_KEY = "T4sGHCKswFjoPjiyBSbq6MIxCcrGPAuHlKslFQDO"
